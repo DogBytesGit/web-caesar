aa68ece43bc26a8a6b64f8734dd0a64e062c3d38
John Sirois <JPSirois@verizon.net>
2017-01-25 22:54:02 -0500

    Added inline css

--- main.py
+++ main.py
@@ -16,16 +16,64 @@
 #
 import webapp2
 import caesar
+import cgi
 
 def build_page(textarea_content):
-    rot_label = "<label>Rotate by:</label>"
-    rotation_input = "<input type='number' name='rotation' value='0'/>"
-    message_label ="<label>Type a message:</label>"
+    head = """
+    <head>
+<style>
+body {
+    background-color: linen;
+}
+
+h2 {
+    color: maroon;
+    font-size: 42px;
+    text-align: center;
+} 
+label {
+    display: block;
+    text-align: center;
+    font-size: 26px;
+    color: maroon;
+    font-weight: bold;
+}
+input {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+textarea {
+    display: block;
+    width: 400;
+    height: 200;
+    margin-left: auto;
+    margin-right: auto;
+    font-size:26px;
+}
+#rot {
+    text-align: center;
+    width: 80px;
+    height: 40px;
+    font-size: 26px;
+}
+#sub {
+    width: 80px;
+    height: 40px;
+    font-size: 16px;
+}
+</style>
+</head>"""
+
+    foot = "</body></html>"
+    rot_label = "<label>Rotate:</label>"
+    rotation_input = "<input type='number' min='0' max='25' id='rot' name='rotation' value='0'/>"
+    message_label ="<label>Enter a message:</label>"
     textarea = "<textarea name='message'>" + textarea_content + "</textarea>"
-    submit = "<input type='submit' value='Encrypt'/>"
+    submit = "<input type='submit' id='sub' value='Encrypt'/>"
     form = "<form method='post'>" + rot_label + rotation_input + "<br>" + message_label + textarea + "<br>" + submit + "</form>"
-    header = "<h2>Web Caesar</h2>"
-    return header + form
+    header = head + "<h2>Web Caesar</h2>"
+    return header + form + foot
 	
 class MainHandler(webapp2.RequestHandler):
 
@@ -37,9 +85,12 @@ class MainHandler(webapp2.RequestHandler):
         message = self.request.get("message")
         message = str(message)
         rotation = self.request.get("rotation")
+        if rotation == "":
+            rotation = 0
         rotation = int(rotation)
         encrypted_message = caesar.encrypt(message,rotation)
-        content = build_page(encrypted_message)
+        escaped_message = cgi.escape(encrypted_message)
+        content = build_page(escaped_message)
         self.response.write(content)
     	
 app = webapp2.WSGIApplication([
